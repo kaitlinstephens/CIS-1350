848345a0af48a1a21d92ba2311b9350ca419a70c
kaitlinstephens <stephens.kaitlin@gmail.com>
2015-09-16 17:21:28 -0400

    Update modernizr-2.js

--- tutorial2/review/modernizr-2.js
+++ tutorial2/review/modernizr-2.js
@@ -3,31 +3,38 @@
   <head>
 	<script src="modernizr-2.js"></script>
 	<title></title>
-	
+
    </head>
+
+   <!--
+        CIS 1350 - Tutorial 1
+        Author: Kaitlin Stephens
+        Date:0902015
+          -->
+
    <body>
-    
+
    <h1>Koalas</h1>
-   
-   
+
+
    <p>
-		
-		<img src = "images/koala_sm.jpg" alt = "Koala Bear" />
-	
+
+		<img src = "koala.jpg" alt = "Koala Bear" />
+
    </p>
-   
+
    <p>
-	Physical Description
-	| Location
-	| Information
+	<a href="html_images.asp">Physical Description</a>
+	| <a href="html_images.asp">Location</a>
+	| <a href="html_images.asp">Information</a>
    </p>
-   
-   
+
+
    <h2>Physical Description</h2>
-	 
-	   
+
+
   <p>The Koala Bear ranges in weight from 11 lbs to 26 lbs. It has 5 fingers, including 2 opposable thumbs, and long claws. The koala has fingerprints, similar to human fingerprints. It is a generally silent animal but may give a loud cry when under stress.</p>
-	   
+
   <p>Like kangaroo babies, baby koalas are called joeys, and they remain in the mothers' pouches for up to six months. When a joey is born, it has no fur or ears, and it is blind. It may be only 1/4 inch long at birth. At about 22 weeks, it will begin to see and will look out of the pouch. After a joey leaves the pouch, it may ride on its mother's back for a while.
 	</p>
 
@@ -37,23 +44,23 @@
 
 
   <h2>Location</h2>
-   
+
   <p>Koalas live in eastern Australia. They require large corridors of connected forest. The habitat of koalas is threatened by urbanization. Koalas live in eucalyptus tree forests. Koalas eat the eucalyptus leaves and most of the water in their diets comes from the leaves or water that is on the leaves.
   </p>
-   
-   
+
+
    <h2>Information</h2>
-   
-   
+
+
 	<ul>
-		<li>Australian Koala Foundation</li>
-		<li>Wikipedia - Koalas</li>
-		<li>National Geographic - Koalas</li>
+		<li><a href="https://www.savethekoala.com/">Australian Koala Foundation</a></li>
+		<li><a href="https://en.wikipedia.org/wiki/Koala">Wikipedia - Koalas</a></li>
+		<li><a href="http://animals.nationalgeographic.com/animals/mammals/koala/">National Geographic - Koalas</a></li>
 	</ul>
-	
-	
+
+
 	<p>Go to Top</p>
-   
-   
+
+
    </body>
-  </html> 
+  </html>
